e97c152099b191ea9cdb5ee1bb626076e1413103
Niels Holt <nho@fcoo.dk>
2020-02-25 09:29:21 +0100

    Adjust to fcoo-settings^2.0 Adding content to modal-form with global settings Adjust demo

--- src/fcoo-latlng-format.js
+++ src/fcoo-latlng-format.js
@@ -8,7 +8,7 @@
 
 ****************************************************************************/
 
-(function ($, window/*, document, undefined*/) {
+(function ($, window, document, undefined) {
 	"use strict";
 
 	//Create fcoo-namespace
@@ -18,7 +18,7 @@
     /***********************************************************
     Set up and load latlng-format via fcoo.settings
     ***********************************************************/
-    ns.settings.add({
+    ns.globalSetting.add({
         id          : 'latlng',
         validator   : function( /*latlngFormatId*/ ){
                           //TODO Check for valid value
@@ -28,15 +28,62 @@
                           window.latLngFormat.setFormat( latlngFormatId );
                       },
         defaultValue: window.latLngFormat.LATLNGFORMAT_DMSS,
-        callApply   : true,
-        globalEvents: window.fcoo.events.LATLNGFORMATCHANGED
+//HER        callApply   : true,
+//HER        globalEvents: ns.events.LATLNGFORMATCHANGED
 
     });
 
 
     //Update the latlng-format when the number-format is changed
     ns.events.on( window.fcoo.events.NUMBERFORMATCHANGED, function(){
-        ns.settings.set('latlng', window.latLngFormat.options.formatId );
+        ns.globalSetting.set({latlng: window.latLngFormat.options.formatId} );
+    });
+
+
+    //Create content for modal-form with settings
+    var items = [];
+    var dC = window.latLngFormat.options.degreeChar;
+    $.each([
+        //Degrees Minutes Seconds Decimal Seconds: N65d30'15.3"  d='degree sign'
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_DMSS,
+            text: {da:'Længde/Breddegrader <span class="text-monospace">(G'+dC+'M S.s)</span>', en:'Longitude/Latitude <span class="text-monospace">(D'+dC+'M\'S.s")</span>'}
+        },
+        //Degrees Decimal minutes                : N65d30.258'
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_DMM,
+            text: {da:'Længde/Breddegrader <span class="text-monospace">(G'+dC+'M.mmm\')</span>', en:'Longitude/Latitude <span class="text-monospace">(D'+dC+'M.mmm\')</span>'}
+        },
+        //Decimal degrees                        : N41.1234d
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_DD,
+            text: {da:'Længde/Breddegrader <span class="text-monospace">(G.gggg'+dC+')</span>', en:'Longitude/Latitude <span class="text-monospace">(D.dddd'+dC+')</span>'}
+        },
+        //UTM                                    : 29Q 286657 2492164
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_UTM,
+            text: 'Universal Transverse Mercator (UTM)'
+        },
+        //MGRS                                   : 02U PG 03727 09686
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_MGRS,
+            text: 'Military Grid Reference System (MGRS)'
+        },
+        //NAC                                    : HBV6R RG77T.
+        {
+            id  : window.latLngFormat.LATLNGFORMAT_NAC,
+            text: 'Natural Area Code (NAC)'
+        }],
+        function(index, info){
+            if (info.id !== undefined)
+                items.push(info);
+        }
+    );
+
+    ns.globalSetting.addModalContent(ns.events.LATLNGFORMATCHANGED, {
+        id   : 'latlng',
+        type : 'selectlist',
+        items: items
     });
 
 }(jQuery, this, document));
